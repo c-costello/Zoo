41dc2c0f7754604c3934ec56f15ca94c7fa4c26c
Clarice Costello <claricostello@outlook.com>
2019-01-14 15:01:18 -0800

    Created Animal and Fish classes

---
+++ Zoo/Zoo/classes/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.classes
+{
+   public abstract class Animal
+    {
+        abstract public string Habitat { get; set; }
+        abstract public string Diet { get; set; }
+
+        abstract public void Sleep();
+        abstract public void Eat();
+        virtual public void Play()
+        {
+            Console.WriteLine("Plays with friends");
+        }
+
+    }
+}
---
+++ Zoo/Zoo/classes/Fish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.classes
+{
+    abstract class Fish : Animal
+    {
+        abstract public string WaterType { get; set; }
+        abstract public int NumberOfGills { get; set; }
+
+        abstract public void Swim();
+    }
+}
