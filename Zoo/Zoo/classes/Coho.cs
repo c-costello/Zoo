1a2caf3a5a1bde2a020241ff078ea1e3d9591584
Clarice Costello <claricostello@outlook.com>
2019-01-14 15:37:18 -0800

    Created Salmon Class

--- Zoo/Zoo/classes/Coho.cs
+++ Zoo/Zoo/classes/Coho.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Zoo.classes
 {
-    class Coho
+    class Coho : Salmon
     {
     }
 }
--- Zoo/Zoo/classes/Salmon.cs
+++ Zoo/Zoo/classes/Salmon.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Zoo.classes
 {
-    class Salmon
+    abstract class Salmon : Fish
     {
+
+        public virtual int RunCycleYears { get; set;} 
+        public override string Diet { get => Diet; set => Diet = "Herring, Squid"; }
+        public override string WaterType { get => WaterType; set => WaterType = "Fresh/Salt"; }
+        public override int NumberOfGills { get => NumberOfGills; set => NumberOfGills = 2; }
+
+        public abstract void Migrate();
     }
 }
