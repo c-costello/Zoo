f52c6a5a21fc20a47a2b7e03fd52901844058c75
Clarice Costello <claricostello@outlook.com>
2019-01-14 15:04:27 -0800

    created mammal class

--- Zoo/Zoo/classes/Mammal.cs
+++ Zoo/Zoo/classes/Mammal.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Zoo.classes
 {
-    class Mammal
+    abstract class Mammal : Animal
     {
+        abstract public int LitterSize { get; set; }
+        virtual public int LivesInGroups { get; set; }
+
+        virtual public void Nurse()
+        {
+            Console.WriteLine("Nursing");
+        }
+
+
     }
 }
