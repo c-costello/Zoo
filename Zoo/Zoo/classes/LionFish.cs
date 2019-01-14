5cf47756e1c4042956e74b534490614b332c0518
Clarice Costello <claricostello@outlook.com>
2019-01-14 15:32:01 -0800

    Created LionFish class

--- Zoo/Zoo/classes/LionFish.cs
+++ Zoo/Zoo/classes/LionFish.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Zoo.classes
 {
-    class LionFish
+    class LionFish : Fish
     {
+        public override string WaterType { get => WaterType; set => WaterType = "Salt"; }
+        public override int NumberOfGills { get => NumberOfGills; set => NumberOfGills = 4; }
+        public override string Habitat { get => Habitat; set => Habitat = "Ocean"; }
+        public override string Diet { get => Diet; set => Diet = "Smaller Fish"; }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Speak");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Sleep");
+        }
+
+        public override void Swim()
+        {
+            Console.WriteLine("Swim");
+        }
     }
 }
