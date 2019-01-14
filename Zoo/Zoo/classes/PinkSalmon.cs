1f7413c7c7b2364b188403aaa2aa9b0a4d94939c
Clarice Costello <claricostello@outlook.com>
2019-01-14 15:50:23 -0800

    Created Pink Salmon Class

--- Zoo/Zoo/classes/PinkSalmon.cs
+++ Zoo/Zoo/classes/PinkSalmon.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace Zoo.classes
 {
-    class PinkSalmon
+    class PinkSalmon : Salmon
     {
+        public bool HasHump { get => HasHump; set => HasHump = true;}
+
+        public override string Habitat { get => Habitat; set => Habitat = "Oceans and Streams"; }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eat");
+        }
+
+        public override void Migrate()
+        {
+            Console.WriteLine("Migrate");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Sleep");
+        }
+
+        public override void Swim()
+        {
+            Console.WriteLine("Swim");
+        }
     }
 }
