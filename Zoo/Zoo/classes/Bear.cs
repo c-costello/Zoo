b3128ed56c321454cde9ca59f0654f01ae5f2f81
Clarice Costello <claricostello@outlook.com>
2019-01-14 16:07:59 -0800

    Created Bear Class

--- Zoo/Zoo/classes/Bear.cs
+++ Zoo/Zoo/classes/Bear.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace Zoo.classes
 {
-    class Bear
+    abstract class Bear : Mammal
     {
+        public virtual bool Omnivore { get => Omnivore; set => Omnivore = true }
+
+        public override bool LivesInGroups { get => base.LivesInGroups; set => base.LivesInGroups = false; }
+
+        public virtual void Fish()
+        {
+            Console.WriteLine("Fish");
+        }
     }
 }
