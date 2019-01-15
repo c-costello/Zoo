21e2f74baa928ee1a1f503acd45cda682e95b105
Clarice Costello <claricostello@outlook.com>
2019-01-15 10:55:28 -0800

    called functions in main

--- Zoo/Zoo/Program.cs
+++ Zoo/Zoo/Program.cs
@@ -8,6 +8,21 @@ namespace Zoo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BlackBear blackBear = new BlackBear();
+            Coho coho = new Coho();
+            GreyWolf greyWolf = new GreyWolf();
+            LionFish lionFish = new LionFish();
+            PinkSalmon pinkSalmon = new PinkSalmon();
+            PolarBear polarBear = new PolarBear();
+
+            
+
+            Console.WriteLine(polarBear.Swim());
+            Console.WriteLine(pinkSalmon.Eat());
+            Console.WriteLine(blackBear.Nurse());
+            Console.WriteLine(lionFish.Sleep());
+            Console.WriteLine(greyWolf.Play());
+            Console.WriteLine(coho.Migrate());
         }
     }
 }
--- Zoo/Zoo/classes/PinkSalmon.cs
+++ Zoo/Zoo/classes/PinkSalmon.cs
@@ -12,7 +12,7 @@ namespace Zoo.classes
 
         public override string Eat()
         {
-            return $"Eats herring";
+            return "Eats herring";
         }
 
         public override string Migrate()
@@ -22,12 +22,12 @@ namespace Zoo.classes
 
         public override string Sleep()
         {
-            return $"Sleeps in salt/fresh water";
+            return "Sleeps in salt/fresh water";
         }
 
         public override string Swim()
         {
-            return $"Swims in salt/fresh water";
+            return "Swims in salt/fresh water";
         }
     }
 }
