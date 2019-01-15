45630e9ff24f3bd42697d22e0d41b5ce6b90e3b4
Clarice Costello <claricostello@outlook.com>
2019-01-14 16:17:04 -0800

    created BlackBear class

--- Zoo/Zoo/classes/BlackBear.cs
+++ Zoo/Zoo/classes/BlackBear.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace Zoo.classes
 {
-    class BlackBear
+    class BlackBear : Bear
     {
+        public override int LitterSize { get => LitterSize; set => LitterSize = 3; }
+        public override string Habitat { get => Habitat; set => Habitat = "Forest"; }
+        public override string Diet { get => Diet; set => Diet = "Fruits, roots, bugs and small game"; }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eat");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Sleep");
+        }
+        public override void Nurse()
+        {
+            base.Nurse();
+        }
+        public void Climb()
+        {
+            Console.WriteLine("Climb");
+        }
+
+
     }
 }
